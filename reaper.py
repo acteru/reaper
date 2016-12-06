f2ee1db5b9404a45d09a52260c02a78e742548f7
Philipp Matti <matti@puzzle.ch>
2016-12-06 23:55:31 +0100

    added new function and fix other functions

--- reaper.py
+++ reaper.py
@@ -1,37 +1,49 @@
 import platform
 import subprocess
 import sys
+import os
+import errno
 import yum
+
+# creates repofolders and sync all enabled repositories
 
 def check_platform(repo_path):
     repo_path = repo_path
     os = platform.linux_distribution()
     if "Fedora" in os[0]:
         set_packagemanager = "dnf"
-        set_repo_path = ( "%s/fedora/%s/" % repo_path, os[1] ) 
-    elif "Centos" in os:
+        set_repo_path = ("%s/fedora/%s" % (repo_path, os[1])) 
+    elif "Centos" in os[0]:
         set_packagemanager = "yum"
-        if "7" in os[0]:
-            set_repo_path = ( "'%s'/centos/'%s'/" % repo_path, os[1] )
-        else:
-            set_repo_path = ( "'%s'/centos/'%s'/" % repo_path, os[1] )
+        set_repo_path = ("%s/centos/%s" % (repo_path, os[1]))
+    elif "RedHat" in os[0]:
+        set_packagemanager = "yum"
+        set_repo_path = ("%s/redhat/%s" % (repo_path, os[1]))
     else:
         sys.exit()
     return(set_packagemanager,set_repo_path)
 
-def sync_repos(packagemanager):
+def ensure_repo_dir(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+def sync_repos(packagemanager,set_repo_path):
     if packagemanager == 'dnf':
-        sync_repos_cmd = ( "dnf reposync -p '%s'" % repo_path )
-        sync = subprocess.check_output(sync_repos_cmd, shell=True)
+        sync_repos_cmd = ("dnf reposync -p %s" % (set_repo_path))
+        print sync_repos_cmd
+        #sync = subprocess.check_output(sync_repos_cmd, shell=True)
     else:
-        sync_repos_cmd = ("reposync -p '%s'" % repo_path) 
-        subprocess.check_output(sync_repos_cmd, shell=True)
+        sync_repos_cmd = ("reposync -p %s" % (set_repo_path)) 
+        print sync_repos_cmd
+        #subprocess.check_output(sync_repos_cmd, shell=True)
 
 def main():
-    repo_path = '/var/repo/'
-    packagemanager = (check_platform(repo_path))
-    print packagemanager[0]
-    print packagemanager[1]
-    #sync_repos(packagemanager)
-
+    repo_path = '/var/repo'
+    pathpackage = (check_platform(repo_path))
+    print pathpackage[1]
+    ensure_repo_dir(pathpackage[1])
+    #sync_repos(pathpackage[0],pathpackage[1])
 main()
