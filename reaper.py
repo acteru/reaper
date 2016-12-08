041d1a403555d9ef5400564e7c12f174f13709c6
Philipp Matti <matti@puzzle.ch>
2016-12-08 16:21:13 +0100

    add configfile and function for it

--- reaper.py
+++ reaper.py
@@ -4,8 +4,17 @@ import sys
 import os
 import errno
 import yum
+import yaml
 
-# creates repofolders and sync all enabled repositories
+sys.tracebacklimit = 0
+
+def load_config():
+    with open('config.yaml', 'r') as config_load:
+        try:
+            loaded_config = yaml.load(config_load)
+            return loaded_config
+        except yaml.YAMLError as exc:
+            print exc
 
 def check_platform(repo_path):
     repo_path = repo_path
@@ -41,9 +50,10 @@ def sync_repos(packagemanager,set_repo_path):
         #subprocess.check_output(sync_repos_cmd, shell=True)
 
 def main():
-    repo_path = '/var/repo'
+    role = load_config()['role']
+    repo_path = load_config()['repo_path']
     pathpackage = (check_platform(repo_path))
     print pathpackage[1]
     ensure_repo_dir(pathpackage[1])
-    #sync_repos(pathpackage[0],pathpackage[1])
+    sync_repos(pathpackage[0],pathpackage[1])
 main()
