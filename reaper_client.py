203debb17cbe1d41f66420c6593803f9947dc704
Philipp Matti <matti@puzzle.ch>
2016-12-20 17:21:13 +0100

    refactor to oop reaper-client

--- reaper_client.py
+++ reaper_client.py
@@ -19,49 +19,52 @@ def load_config():
         except yaml.YAMLError as exc:
             print exc
 
+class RepoSync():
 
-def check_platform(repo_path):
-    repo_path = repo_path
-    os = platform.linux_distribution()
-    if "Fedora" in os[0]:
-        packagemanager = "dnf"
-        repo_path = ("%s/fedora/%s" % (repo_path, os[1]))
-    elif "Centos" in os[0]:
-        packagemanager = "yum"
-        repo_path = ("%s/centos/%s" % (repo_path, os[1]))
-    elif "CentOS Linux" in os[0]:
-        packagemanager = "yum"
-        repo_path = ("%s/centos/%s" % (repo_path, os[1]))
-    elif "RedHat" in os[0]:
-        packagemanager = "yum"
-        repo_path = ("%s/redhat/%s" % (repo_path, os[1]))
-    else:
-        sys.exit()
-    return(packagemanager,repo_path)
+    def __init__(self,repo_path):
+        self.repo_path = repo_path
 
-
-def create_repo_dir(path):
-    try:
-        os.makedirs(path)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
+    def check_platform(self,repo_path):
+        repo_path = repo_path
+        os = platform.linux_distribution()
+        if "Fedora" in os[0]:
+            packagemanager = "dnf"
+            repo_path = ("%s/fedora/%s" % (repo_path, os[1]))
+        elif "Centos" in os[0]:
+            packagemanager = "yum"
+            repo_path = ("%s/centos/%s" % (repo_path, os[1]))
+        elif "CentOS Linux" in os[0]:
+            packagemanager = "yum"
+            repo_path = ("%s/centos/%s" % (repo_path, os[1]))
+        elif "RedHat" in os[0]:
+            packagemanager = "yum"
+            repo_path = ("%s/redhat/%s" % (repo_path, os[1]))
+        else:
+            sys.exit()
+        return(packagemanager,repo_path)
 
 
-def sync_repos(packagemanager,repo_path):
-    if packagemanager == 'dnf':
-        sync_repos_cmd = ("dnf reposync -p %s" % (repo_path))
-        print sync_repos_cmd
-        subprocess.check_output(sync_repos_cmd, shell=True)
-    else:
-        sync_repos_cmd = ("reposync -p %s" % (repo_path))
-        print sync_repos_cmd
-        subprocess.check_output(sync_repos_cmd, shell=True)
+    def create_repo_dir(self,path):
+        try:
+            os.makedirs(path)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+               raise
 
 
-def main():
+    def sync_repos(self,packagemanager,repo_path):
+        if packagemanager == 'dnf':
+            sync_repos_cmd = ("dnf reposync -p %s" % (repo_path))
+            print sync_repos_cmd
+            subprocess.check_output(sync_repos_cmd, shell=True)
+        else:
+            sync_repos_cmd = ("reposync -p %s" % (repo_path))
+            print sync_repos_cmd
+            subprocess.check_output(sync_repos_cmd, shell=True)
+
+if __name__ == "__main__":
     repo_path = load_config()['repo_path']
-    pathpackage = (check_platform(repo_path))
-    create_repo_dir(pathpackage[1])
-    sync_repos(pathpackage[0],pathpackage[1])
-main()
+    reposync = RepoSync(repo_path)
+    pathpackage = reposync.check_platform(repo_path)
+    reposync.create_repo_dir(pathpackage[1])
+    reposync.sync_repos(pathpackage[0],pathpackage[1])
